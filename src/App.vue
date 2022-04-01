57cbe5c97cfcf0d651081a6c33dd066534bcba15
dudusohne <dudu.hit@gmail.com>
2022-04-01 11:32:25 -0300

    base components structure

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-
 <template>
   <Home />
 </template>
@@ -8,5 +7,9 @@ import Home from './pages/Home/Home.vue'
 </script>
 
 <style>
-
+body {
+  background-color: #191622;
+  margin: 0;
+  padding: 0;
+}
 </style>
